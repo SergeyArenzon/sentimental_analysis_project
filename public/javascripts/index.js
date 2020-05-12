627c4a2ba571c61fc56432bb35c4dfc4cc752747
SergeyArenzon <sergeyr1991@gmail.com>
2020-05-12 15:42:12 +0300

    add sentiment analysis

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,22 @@
 
 
 
-document.getElementById('button-addon1').addEventListener('click', () => {
-    
+document.getElementById('button-addon1').addEventListener('click', e => {
+    e.preventDefault();
+    var input = document.getElementById('userInput').value;
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify({ input }),
+        headers: new Headers({ 'Content-Type': 'application/json' })
+    }
+
+    fetch('/', options)
+        .then(response => response.json())
+        .then(({analysis}) => {
+            
+            document.getElementById('output').innerHTML = analysis
+        })
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,52 @@
 var express = require('express');
 var router = express.Router();
 
-var Analyzer = require('natural').SentimentAnalyzer;
-var stemmer = require('natural').PorterStemmer;
-var analyzer = new Analyzer("English", stemmer, "afinn");
+const aposToLexForm = require('apos-to-lex-form');
+const natural = require('natural');
+const SpellCorrector = require('spelling-corrector');
+const SW = require('stopword');
+
+
+const spellCorrector = new SpellCorrector();
+spellCorrector.loadDictionary();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
   res.render('index', { input: '' });
-
-
 });
 
 
 
-router.post('/', (req, res) => {  
-  res.render('index', {input: analyzer.getSentiment(["I", "banana", "loved"])});
+// post req for  sentiment analyzing from fetch
+router.post('/', (req, res) => { 
 
+  const { input } = req.body; // get the user input
+  const lexedInput = aposToLexForm(input); // fixes examples: i'am to i am 
+  const casedInput = lexedInput.toLowerCase(); //to lower case
+  const alphaOnlyInput = casedInput.replace(/[^a-zA-Z\s]+/g, ''); //removing non alphabetical
   
+  const { WordTokenizer } = natural;
+  const tokenizer = new WordTokenizer();
+  const tokenizedInput = tokenizer.tokenize(alphaOnlyInput); // tokenize the input string
 
+  tokenizedInput.forEach((word, index) => {
+    tokenizedInput[index] = spellCorrector.correct(word);  // correct spellings
+  })
+  const filteredInput = SW.removeStopwords(tokenizedInput); // removes unrelevant words
 
+  const { SentimentAnalyzer, PorterStemmer } = natural;
+
+  // AFFIN is vocabulary of words rated by -3 to 3
+  // analyzer.getSentimental summing the polarity of each word and normalizing with the length of the sentence
+  const analyzer = new SentimentAnalyzer('English', PorterStemmer, 'afinn'); 
+  const analysis = analyzer.getSentiment(filteredInput); // 
+
+  res.status(200).json({analysis});
+  
 })
+
 
 
 module.exports = router;
