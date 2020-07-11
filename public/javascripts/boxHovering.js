c2d1303ce333e75246bb5250c4aed1f8a53edef5
sergey <sergeyr1991@gmail.com>
2020-07-11 14:49:13 +0300

    add boxHovering.js + add box[123]Div

---
+++ public/javascripts/boxHovering.js
@@ -0,0 +1,15 @@
+
+
+
+let box1 = document.getElementById('box1').addEventListener('mouseover', () => {
+    let box1Div = document.getElementById('box1Div');
+    box1Div.innerText = "sergey";
+});
+
+let box2 = document.getElementById('box2').addEventListener('mouseover', event => {
+    
+});
+
+let box3 = document.getElementById('box3').addEventListener('mouseover', event => {
+    
+});
