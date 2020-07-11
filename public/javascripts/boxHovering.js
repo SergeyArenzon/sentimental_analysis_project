4839bb529e681fac30901e790cf5c8ad0d135f86
sergey <sergeyr1991@gmail.com>
2020-07-11 16:29:05 +0300

    add hover/unhover handler comments

--- public/javascripts/boxHovering.js
+++ public/javascripts/boxHovering.js
@@ -1,12 +1,17 @@
+//------------------------------
+//  BOX HOVER/UNHOVER HANDLER    
+//------------------------------
 
 
 
+// BOX1 HOVER HANDLER
 const box1hovered = document.getElementById('box1').addEventListener('mouseover', () => {
     let box1Div = document.getElementById('box1Div');
     let box1hover = document.getElementById('box1hover');
-    box1hover.style["display"]= "block";
-    
+    box1hover.style["display"]= "block";    
 });
+
+// BOX1 UNHOVER HANDLER
 const box1unHovered = document.getElementById('box1').addEventListener('mouseout', () => {
     let box1Div = document.getElementById('box1Div');
     let box1hover = document.getElementById('box1hover');
@@ -14,10 +19,12 @@ const box1unHovered = document.getElementById('box1').addEventListener('mouseout
     
 });
 
+
+// BOX2 HOVER HANDLER
 const box2 = document.getElementById('box2').addEventListener('mouseover', () => {
     
 });
-
+// BOX3 HOVER HANDLER
 const box3 = document.getElementById('box3').addEventListener('mouseover', () => {
     
 });
