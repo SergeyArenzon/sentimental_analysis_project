024448f3be3f18a01ad5e221fc37b23bb58fa9de
sergey <sergeyr1991@gmail.com>
2020-07-11 16:09:22 +0300

    add hover/unhover events

--- public/javascripts/boxHovering.js
+++ public/javascripts/boxHovering.js
@@ -1,15 +1,23 @@
 
 
 
-let box1 = document.getElementById('box1').addEventListener('mouseover', () => {
+const box1hovered = document.getElementById('box1').addEventListener('mouseover', () => {
     let box1Div = document.getElementById('box1Div');
-    box1Div.innerText = "sergey";
+    let box1hover = document.getElementById('box1hover');
+    box1hover.style["display"]= "block";
+    
 });
-
-let box2 = document.getElementById('box2').addEventListener('mouseover', event => {
+const box1unHovered = document.getElementById('box1').addEventListener('mouseout', () => {
+    let box1Div = document.getElementById('box1Div');
+    let box1hover = document.getElementById('box1hover');
+    box1hover.style["display"]= "none";
     
 });
 
-let box3 = document.getElementById('box3').addEventListener('mouseover', event => {
+const box2 = document.getElementById('box2').addEventListener('mouseover', () => {
+    
+});
+
+const box3 = document.getElementById('box3').addEventListener('mouseover', () => {
     
 });
