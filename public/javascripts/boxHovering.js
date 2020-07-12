a7dff7df7ca7a21dd4c7215291d58938a05d3e7e
sergey <sergeyr1991@gmail.com>
2020-07-12 15:48:07 +0300

    fix box2, remove output forms

--- public/javascripts/boxHovering.js
+++ public/javascripts/boxHovering.js
@@ -8,7 +8,7 @@
 const box1hovered = document.getElementById('box1').addEventListener('mouseover', () => {
     let box1Div = document.getElementById('box1Div');
     let box1hover = document.getElementById('box1hover');
-    box1hover.style["display"]= "block";    
+    box1hover.style["display"]= "block";
 });
 
 // BOX1 UNHOVER HANDLER
@@ -22,8 +22,19 @@ const box1unHovered = document.getElementById('box1').addEventListener('mouseout
 
 // BOX2 HOVER HANDLER
 const box2 = document.getElementById('box2').addEventListener('mouseover', () => {
+    let box2Div = document.getElementById('box2Div');
+    let box2hover = document.getElementById('box2hover');
+    box2hover.style["display"]= "block";
     
 });
+
+const box2unHovered = document.getElementById('box2').addEventListener('mouseout', () => {
+    let box2Div = document.getElementById('box2Div');
+    let box2hover = document.getElementById('box2hover');
+    box2hover.style["display"]= "none";
+    
+});
+
 // BOX3 HOVER HANDLER
 const box3 = document.getElementById('box3').addEventListener('mouseover', () => {
     
