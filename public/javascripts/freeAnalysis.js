3fa9196eedfcd1fb8d5801b68c27b0c8d88b623c
sergey <sergeyr1991@gmail.com>
2020-08-20 16:55:50 +0300

    add neutral face to free text feedbacks

--- public/javascripts/freeAnalysis.js
+++ public/javascripts/freeAnalysis.js
@@ -13,7 +13,7 @@ hideDivs = () => {
 
 const happySmiley = '<div class="smiley"><div class="smiley-face smiley-face--happy"><div class="smiley-eyes smiley-eyes--happy"><div class="smiley-eye"></div><div class="smiley-eye"></div></div><div class="smiley-mouth smiley-mouth--happy"><div class="smiley-tongue"></div></div></div></div>';
 const sadSmiley = '<div class="smiley smiley--sad"><div class="smiley-face smiley-face--sad"><div class="smiley-eyes smiley-eyes--sad"><div class="smiley-eye"></div><div class="smiley-eye"></div></div><div class="smiley-mouth smiley-mouth--sad"></div></div></div>';
-
+const neutralSmiley = '<div class="smiley"><div class="smiley-face"><div class="smiley-eyes smiley-eyes--neutral"><div class="smiley-eye"></div><div class="smiley-eye"></div></div><div class="smiley-mouth smiley-mouth--neutral"><div class="smiley-mouth-sad--neutral"><div class="row"><div class="column"></div><div class="column"></div></div></div></div></div></div>';
 document.getElementById("freeTextConfirm").addEventListener('click', e => {
     e.preventDefault();
 
@@ -33,7 +33,7 @@ document.getElementById("freeTextConfirm").addEventListener('click', e => {
             document.getElementById('freeDiv').style.display = 'inline';
             if(analysis > 0 ) {document.getElementById('feedbackSmiley').innerHTML = happySmiley;}
             else if(analysis < 0) {document.getElementById('feedbackSmiley').innerHTML = sadSmiley;}
-    
+            else {document.getElementById('feedbackSmiley').innerHTML = neutralSmiley;}
             document.getElementById('freeText_avg').innerHTML = analysis;
             document.getElementById('freeText_pos_num').innerHTML = pos_num;
             document.getElementById('freeText_neg_num').innerHTML = neg_num;
