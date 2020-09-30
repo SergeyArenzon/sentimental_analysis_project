8d941bc370514166a54496f98cf905ed57be5051
SergeyArenzon <sergeyr1991@gmail.com>
2020-09-30 13:59:53 +0300

    add output div animatiopn

--- public/javascripts/titleText.js
+++ public/javascripts/titleText.js
@@ -13,6 +13,7 @@ function changeWord() {
     for (var i = 0; i < cw.length; i++) {
     animateLetterOut(cw, i);
     }
+    
     for (var i = 0; i < nw.length; i++) {
         nw[i].className = 'letter behind';
         nw[0].parentElement.style.opacity = 1;
